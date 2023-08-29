b8e9e6f3ae9025d459f85ce7cc6d3d70a3355aae
sreejakatta14 <138003232+sreejakatta14@users.noreply.github.com>
2023-08-29 20:27:56 +0530

    Update program2

--- program2.c
+++ program2.c
@@ -1,176 +1,236 @@
-#include<stdio.h>
-    int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10], q[10];  
-    float avg_wt, avg_tat;  
-void roundrobin()
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_PROCESSES 10
+
+struct Process
 {
-printf("Enter the Time Quantum for the process: \t");  
-scanf("%d", &quant);  
-}
-void rr()  
-{  
-   
-
-printf(" Total number of process in the system: ");  
-scanf("%d", &NOP);  
-y = NOP;
- 
-
-for(i=0; i<NOP; i++)  
-{  
-printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
-printf(" Arrival time is: \t");
-scanf("%d", &at[i]);  
-printf(" \nBurst time is: \t");  
-scanf("%d", &bt[i]);  
-temp[i] = bt[i];
-}  
+    int pid;
+    int arr_time;
+    int burst_time;
+    int priority;
+    int rem_time;
+    int tat;
+    int wt;
+};
 
 
-
-printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");  
-for(sum=0, i = 0; y!=0; )  
-{  
-if(temp[i] <= quant && temp[i] > 0)
-{  
-    sum = sum + temp[i];  
-    temp[i] = 0;  
-    count=1;  
-    }    
-    else if(temp[i] > 0)  
-    {  
-        temp[i] = temp[i] - quant;  
-        sum = sum + quant;    
-    }  
-    if(temp[i]==0 && count==1)  
-    {  
-        y--;
-        printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
-        wt = wt+sum-at[i]-bt[i];  
-        tat = tat+sum-at[i];  
-        count =0;    
-    }  
-    if(i==NOP-1)  
-    {  
-        i=0;  
-    }  
-    else if(at[i+1]<=sum)  
-    {  
-        i++;  
-    }  
-    else  
-    {  
-        i=0;  
-    }  
-}  
-
-avg_wt = wt * 1.0/NOP;  
-avg_tat = tat * 1.0/NOP;  
-printf("\n Average Turn Around Time: \t%f", avg_wt);  
-printf("\n Average Waiting Time: \t%f", avg_tat);  
- 
-}  
-void non()
+void priority_nonpreemptive(struct Process p[], int n)
 {
-    int pn = 0;                
-    int CPU = 0;            
-    int allTime = 0;      
-    printf("Enrer Processes Count: ");
-    scanf("%d",&pn);
-    int AT[pn];
-    int ATt[pn];
-    int NoP = pn;
-    int PT[pn];            
-    int PP[pn];             
-    int PPt[pn];
-    int waittingTime[pn];
-    int turnaroundTime[pn];
-    int i=0;
-    for(i=0 ;i<pn ;i++){
-        printf("\nProcessing time for P%d: ",i+1);
-        scanf("%d",&PT[i]);
-        printf("Piriorty for P%d: ",i+1);
-        scanf("%d",&PP[i]);
-        PPt[i] = PP[i];
-        printf("Arrival Time for P%d: ",i+1);
-        scanf("%d",&AT[i]);
-        ATt[i] = AT[i];
+    int i, j, count = 0, m;
+    for (i = 0; i < n; i++)
+    {
+        if (p[i].arr_time == 0)
+            count++;
     }
-    int LAT = 0;       
-    for(i = 0; i < pn; i++)
-        if(AT[i] > LAT)
-            LAT = AT[i];
-
-    int MAX_P = 0;        
-    for(i = 0; i < pn; i++)
-        if(PPt[i] > MAX_P)
-            MAX_P = PPt[i];
-    int ATi = 0;        
-    int P1 = PPt[0];   
-    int P2 = PPt[0];     
-    int j = -1;
-    while(NoP > 0 && CPU <= 1000){
-        for(i = 0; i < pn; i++){
-            if((ATt[i] <= CPU) && (ATt[i] != (LAT+10))){
-                if(PPt[i] != (MAX_P+1)){
-                    P2 = PPt[i];
-                    j= 1;
-
-                    if(P2 < P1){
-                        j= 1;
-                        ATi = i;
-                        P1 = PPt[i];
-                        P2 = PPt[i];
+    if (count == n || count == 1)
+    {
+        if (count == n)
+        {
+            for (i = 0; i < n - 1; i++)
+            {
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (p[j].priority > p[j + 1].priority)
+                    {
+                        struct Process temp = p[j];
+                        p[j] = p[j + 1];
+                        p[j + 1] = temp;
                     }
                 }
             }
         }
 
-        if(j == -1){
-            CPU = CPU+1;
-            continue;
-        }else{
-
-
-            waittingTime[ATi] = CPU - ATt[ATi];
-            CPU = CPU + PT[ATi];
-            turnaroundTime[ATi] = CPU - ATt[ATi];
-            ATt[ATi] = LAT +10;
-            j = -1;
-            PPt[ATi] = MAX_P + 1;
-            ATi = 0;       
-            P1 = MAX_P+1;     
-            P2 = MAX_P+1;   
-            NoP = NoP - 1;
-
+        else
+        {
+            for (i = 1; i < n - 1; i++)
+            {
+                for (j = 1; j <= n - i - 1; j++)
+                {
+                    if (p[j].priority > p[j + 1].priority)
+                    {
+                        struct Process temp = p[j];
+                        p[j] = p[j + 1];
+                        p[j + 1] = temp;
+                    }
+                }
+            }
         }
     }
-    printf("\nPN\tPT\tPP\tAT\tWT\tTT\n\n");
-    for(i = 0; i < pn; i++){
-       printf("P%d\t%d\t%d\t%d\t%d\t%d\n",i+1,PT[i],PP[i],AT[i],waittingTime[i],turnaroundTime[i]);
+
+    int total_time = 0;
+    double total_tat = 0;
+    double total_wt = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        total_time += p[i].burst_time;
+        p[i].tat = total_time - p[i].arr_time;
+        p[i].wt = p[i].tat - p[i].burst_time;
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
     }
 
-    float AvgWT = 0;
-    float AVGTaT = 0;
-    for(i = 0; i < pn; i++){
-        AvgWT = (float)waittingTime[i] + AvgWT;
-        AVGTaT = (float)turnaroundTime[i] + AVGTaT;
-    }
-    printf("AvgWaittingTime = %f\nAvgTurnaroundTime = %f\n",AvgWT/pn,AVGTaT/pn);
-}
-void main()
-{
-    int ch;
-    printf("Enter choice 1-priority(non premetive) 2-Priority(Premetive) 3-Round Robin: ");
-    scanf("%d",&ch);
-    switch(ch)
+    printf("Process\tTurnaround Time\tWaiting Time\n");
+    for (i = 0; i < n; i++)
     {
-        case 1:
-        non();
-        break;
-        case 3:
-        rr();
-        break;
-        default:
-        Printf("Wrong choice");
+        printf("%d\t%d\t\t%d\n", p[i].pid, p[i].tat, p[i].wt);
     }
+
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+}
+
+void priority_preemptive(struct Process p[], int n)
+{
+    int total_time = 0, i;
+    int completed = 0;
+
+    while (completed < n)
+    {
+        int highest_priority = -1;
+        int next_process = -1;
+
+        for (i = 0; i < n; i++)
+        {
+            if (p[i].arr_time <= total_time && p[i].rem_time > 0)
+            {
+                if (highest_priority == -1 || p[i].priority < highest_priority)
+                {
+                    highest_priority = p[i].priority;
+                    next_process = i;
+                }
+            }
+        }
+
+        if (next_process == -1)
+        {
+            total_time++;
+            continue;
+        }
+
+        p[next_process].rem_time--;
+        total_time++;
+
+        if (p[next_process].rem_time == 0)
+        {
+            completed++;
+            p[next_process].tat = total_time - p[next_process].arr_time;
+            p[next_process].wt = p[next_process].tat - p[next_process].burst_time;
+        }
+    }
+
+    double total_tat = 0;
+    double total_wt = 0;
+
+    printf("Process\tTurnaround Time\tWaiting Time\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t\t%d\n", p[i].pid, p[i].tat, p[i].wt);
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
+    }
+
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+}
+
+void round_robin(struct Process p[], int n, int quantum)
+{
+    int total_time = 0, i;
+    int completed = 0;
+
+    printf("\nGantt Chart: \n");
+    while (completed < n)
+    {
+
+        for (i = 0; i < n; i++)
+        {
+            if (p[i].arr_time <= total_time && p[i].rem_time > 0)
+            {
+                if (p[i].rem_time <= quantum)
+                {
+                    printf("P%d ", p[i].pid);
+                    total_time += p[i].rem_time;
+                    p[i].rem_time = 0;
+                    p[i].tat = total_time - p[i].arr_time;
+                    p[i].wt = p[i].tat - p[i].burst_time;
+                    completed++;
+                }
+                else
+                {
+                    printf("P%d ", p[i].pid);
+                    total_time += quantum;
+                    p[i].rem_time -= quantum;
+                }
+            }
+        }
+    }
+
+    double total_tat = 0;
+    double total_wt = 0;
+
+    printf("\n");
+    printf("\nProcess\tTurnaround Time\tWaiting Time\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t\t%d\n", p[i].pid, p[i].tat, p[i].wt);
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
+    }
+
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+}
+
+int main()
+{
+    int n, quantum, i, choice;
+    struct Process p[MAX_PROCESSES];
+
+    printf("Enter the number of Processes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("\nFor Process %d\n", i + 1);
+        printf("Enter Arrival time, Burst Time, Priority:\n");
+        scanf("%d%d%d",&p[i].arr_time,&p[i].burst_time,&p[i].priority);
+        p[i].pid = i + 1;
+        p[i].rem_time = p[i].burst_time;
+        p[i].tat = 0;
+        p[i].wt = 0;
+    }
+
+    printf("\nSelect a scheduling algorithm:\n");
+
+    printf("1. Priority (Preemtive & Non-preemptive)\n");
+    printf("2. Round Robin\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("\n>> Priority Non-preemptive Scheduling:\n");
+        priority_nonpreemptive(p, n);
+
+        printf("\n>> Priority Preemptive Scheduling:\n");
+        priority_preemptive(p, n);
+        break;
+    case 2:
+        printf("\nEnter the quantum size for Round Robin: ");
+        scanf("%d", &quantum);
+        printf("\n>> Round Robin Scheduling (Quantum: %d):\n", quantum);
+        round_robin(p, n, quantum);
+        break;
+    default:
+        printf("Invalid choice!\n");
+        return 1;
+    }
+
+    return 0;
 }
